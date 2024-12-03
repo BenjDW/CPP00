b63e0bfc3ff3455d8e63f18d947919714f1c18c2
vm_ubuntu42 <dewitskibenji@live.fr>
2024-12-03 10:30:17 -0800

    function search in progress , need to debug all

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,37 +6,91 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 09:07:32 by bde-wits          #+#    #+#             */
-/*   Updated: 2024/12/03 06:57:49 by bde-wits         ###   ########.fr       */
+/*   Updated: 2024/12/03 10:28:29 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "class.hpp"
 #include <iostream>
 #include <string>
+#include <iomanip>
 
-// recupere la value associe dans le phonebook et la renvoie
-// std::string		getvar(int pos)
-// {
+// SEARCH : affiche le contact demandé
+// ◦ Affiche les contacts enregistrés sous la forme d’une liste de 4 colonnes : index,
+// first name, last name et nickname.
+// ◦ Chaque colonne doit faire 10 caractères de long. Elles doivent être séparées
+// par un pipe (’|’). Leur texte est aligné à droite. Si le texte dépasse la largeur
+// de la colonne, il faut le tronquer et remplacer le dernier caractère affiché par
+// un point (’.’).
+// ◦ Ensuite, le programme demande à l’utilisateur d’entrer l’index du contact à afficher. Si l’index ou son format sont incorrects, gérez cela de manière pertinente.
+// Sinon, affichez les informations du contact, une par ligne.
 
-// 	return ();
-// }
-
-// char	*setvar(std::string *str, int pos)
-// {
-	
-// }
-
-// void	search(phone_book repertoire, int index)
-// {
-// 	if (repertoire. == NULL)
-// 		std::cout << "aucun contact dans le repertoire" << std::endl;
-// 	std::cout << repertoire.getvar(index, 0) << std::endl;
-// }
-
-std::string	Contact::getvar(int index, int type) //getter
+void	phone_book::search()
 {
-	if (index < 0 || index > 7)
-		return (NULL);
+	std::string	solo_contact;
+	int			solo;
+
+	if (count == 0) {
+        std::cout << "No contacts available." << std::endl;
+        return;
+    }
+    // Afficher l'en-tête // setw = set width
+    std::cout << std::setw(10) << "Index" << "|"
+              << std::setw(10) << "First Name" << "|"
+              << std::setw(10) << "Last Name" << "|"
+              << std::setw(10) << "Nickname" << std::endl;
+    std::cout << std::string(44, '-') << std::endl;  // Ligne de séparation
+    // Afficher les contacts existants (limité à 8)
+    for (int i = 0; i < count && i < 8; i++)
+	{
+        std::cout << std::setw(10) << i << "|"
+                  << std::setw(10) << trunc(contact[i]->getvar(0)) << "|"
+                  << std::setw(10) << trunc(contact[i]->getvar(1)) << "|"
+                  << std::setw(10) << trunc(contact[i]->getvar(2)) << std::endl;
+    }
+	while (1)
+	{
+		std::cout << "entre l'index du contact : ";
+		std::getline(std::cin, solo_contact);
+		if (all_number(solo_contact) == 0)
+		{
+			solo = std::stoi(solo_contact);
+			if (solo > count || solo > 7 || solo < 0)
+				std::cout << "need a valid index (0 to 7), overflow" << std::endl;
+			break ;
+		}
+		else
+			std::cout << "need only number" << std::endl;
+	}
+	std::cout << "First Name: " << contact[solo]->getvar(0) << std::endl;
+	std::cout << "Last Name: " << contact[solo]->getvar(1) << std::endl;
+	std::cout << "Nickname: " << contact[solo]->getvar(2) << std::endl;
+	std::cout << "Phone Number: " << contact[solo]->getvar(3) << std::endl;
+	std::cout << "Darkest Secret: " << contact[solo]->getvar(4) << std::endl;
+}
+
+int	all_number(std::string index)
+{
+	int i = -1;
+
+	while (index[++i] != '\n')
+	{
+		if (!isdigit(index[i]))
+			return (1);
+	}
+	return (0);
+}
+
+// Fonction pour tronquer une chaîne à 10 caractères
+std::string trunc(const std::string& str)
+{
+    if (str.length() > 10)
+        return str.substr(0, 9) + ".";  // Tronquer et ajouter un point
+    return str;
+}
+
+std::string	Contact::getvar(int type) //getter
+{
 	if (type == 0)
 		return (firstname);
 	if (type == 1)
@@ -49,10 +103,8 @@ std::string	Contact::getvar(int index, int type) //getter
 		return (darkest_secret);
 }
 
-void	Contact::setvar(int index, int type, std::string info) //setter
+void	Contact::setvar(int type, std::string info) //setter
 {
-	if (index < 0 || index > 7)
-		return ;
 	if (type == 0)
 		this->firstname = info; //.c_str();
 	if (type == 1)
@@ -68,29 +120,28 @@ void	Contact::setvar(int index, int type, std::string info) //setter
 void	phone_book::add(std::string temp)
 {
 	Contact	*contact = new Contact;
-	int	index = 0;
 	int icpy = this->count % 8;
 
-	while(1)
-	{
-		std::getline(std::cin, temp, '\n');
-		switch (index)
-		{
-			case 0:
-				contact->setvar(icpy, index, temp);
-			case 1:
-				contact->setvar(icpy, index, temp);
-			case 2:
-				contact->setvar(icpy, index, temp);
-			case 3:
-				contact->setvar(icpy, index, temp);
-			case 4:
-				contact->setvar(icpy, index, temp);
-			case 5:
-				break;
-		}
-		index++;
-	}
+	std::cout << "First Name: " << std::endl;
+    std::getline(std::cin, temp);
+    contact->setvar(0, temp);
+    
+    std::cout << "Last Name: " << std::endl;
+    std::getline(std::cin, temp);
+    contact->setvar(1, temp);
+
+    std::cout << "Nickname: " << std::endl;
+    std::getline(std::cin, temp);
+    contact->setvar(2, temp);
+
+    std::cout << "Phone Number: " << std::endl;
+    std::getline(std::cin, temp);
+    contact->setvar(3, temp);
+
+    std::cout << "Darkest Secret: " << std::endl;
+    std::getline(std::cin, temp);
+    contact->setvar(4, temp);
+
 	this->contact[icpy] = contact;
 	this->count += 1;
 }
